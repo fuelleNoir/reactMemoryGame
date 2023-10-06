ba1f0d6d9106fd8949b4814980425069826cdeaf
katia <zanudakatia0@gmail.com>
2023-10-06 12:02:00 +0300

    commit

---
+++ settings.js
@@ -0,0 +1,16 @@
+const TIMEOUT = 1000;
+
+const AppRoute = {
+    Initial: 'initial',
+    Game: 'game',
+    Results: 'results',
+  };
+  
+  const GAME_TYPES = [
+   { type: 'cats', text: 'котики' },
+   { type: 'cars', text: 'машины' },
+    { type: 'flowers', text: 'цветочки'},
+  ];
+
+const RANDOMIZED = true;
+window.TIMEOUT = TIMEOUT;
